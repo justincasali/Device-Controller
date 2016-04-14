1aa3485c56e407e56291b916f1592dd4f743f73a
Charlie King <charlie_king4967@hotmail.com>
2016-04-14 14:27:44 -0600

    Started Time
    Gettin’ there!

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -1,4 +1,5 @@
 package ecen.devicecontroller;
+import java.util.Calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR);
+        int min = cal.get(Calendar.MINUTE);
     }
 }
