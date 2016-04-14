8089b3efc6137a66bd7bfb0d95e3538eeec232ef
Charlie King <charlie_king4967@hotmail.com>
2016-04-14 15:50:31 -0600

    Working Time Display

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -3,8 +3,10 @@ import java.util.Calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
         Calendar cal = Calendar.getInstance();
         int hour = cal.get(Calendar.HOUR);
         int min = cal.get(Calendar.MINUTE);
+        text = (TextView) findViewById(R.id.textView2);
+        text.setText(hour+":"+min);
+
     }
 }
