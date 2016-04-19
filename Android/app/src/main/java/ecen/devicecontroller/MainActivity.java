32275a89acd000dbdb7e0c7a5104eb37560d69e0
justincasali <justin.casali@colorado.edu>
2016-04-19 16:05:36 -0600

    Work in progress

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -1,13 +1,51 @@
 package ecen.devicecontroller;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.provider.Telephony;
+
+import android.content.BroadcastReceiver;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    public BroadcastReceiver broadcast_receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //String message = Telephony.Sms.Intents.getMessagesFromIntent(intent)[0].getMessageBody();
+            //text.setText(message);
+            if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+                String m = "Text Message";
+                text.setText(m);
+            }
+        }
+    };
+
+    public IntentFilter intent_filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textMessage);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.registerReceiver(broadcast_receiver, intent_filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(broadcast_receiver);
+    }
+
 }
