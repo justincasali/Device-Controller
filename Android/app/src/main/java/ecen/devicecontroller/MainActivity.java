a14aa60435c4b4c716db2a34b284e5dde1e2b963
penguinpajamas <CharlieKing@Charlies-MacBook-Pro.local>
2016-04-19 15:29:37 -0600

    Added phone number recognition

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -3,10 +3,16 @@ import java.util.Calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
 import android.widget.TextView;
+import android.telephony.TelephonyManager;
+import android.app.Service;
+
+
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +22,20 @@ public class MainActivity extends AppCompatActivity {
         int hour = cal.get(Calendar.HOUR_OF_DAY);
         int min = cal.get(Calendar.MINUTE);
         text = (TextView) findViewById(R.id.textView2);
+        text2 = (TextView) findViewById(R.id.textView);
         text.setText(hour+":"+min);
 
+        TelephonyManager manager = (TelephonyManager) getSystemService(Service.TELEPHONY_SERVICE);
+        manager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
     }
+
+    private PhoneStateListener listener = new PhoneStateListener(){
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber){
+            if (state == TelephonyManager.CALL_STATE_RINGING){
+                text2.setText(incomingNumber);
+            }
+        }
+    };
+
 }
