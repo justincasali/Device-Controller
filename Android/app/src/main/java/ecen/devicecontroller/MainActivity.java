e5f5f301d233bda1273713d9fe2ca42b46c04458
justincasali <justin.casali@colorado.edu>
2016-04-21 15:40:14 -0600

    Something that works

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -15,33 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView text;
 
-    public BroadcastReceiver broadcast_receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String message = Telephony.Sms.Intents.getMessagesFromIntent(intent)[0].getMessageBody();
-            text.setText(message);
-        }
-    };
-
-    public IntentFilter intent_filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textMessage);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        this.registerReceiver(broadcast_receiver, intent_filter);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        this.unregisterReceiver(broadcast_receiver);
     }
 
 }
--- Android/app/src/main/java/ecen/devicecontroller/SMSReceiver.java
+++ Android/app/src/main/java/ecen/devicecontroller/SMSReceiver.java
@@ -3,22 +3,45 @@ package ecen.devicecontroller;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import android.provider.Telephony;
+import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
-import android.widget.Toast;
 
-/**
- * Created by justincasali on 4/19/16.
- */
-public class SMSReceiver extends BroadcastReceiver {
+public class SmsReceiver extends BroadcastReceiver {
+    private String TAG = SmsReceiver.class.getSimpleName();
+
+    public SmsReceiver() {
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-        String text = messages[0].getDisplayMessageBody();
-        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
-    }
+        // Get the data (SMS data) bound to intent
+        Bundle bundle = intent.getExtras();
 
+        SmsMessage[] msgs = null;
+
+        String str = "";
+
+        if (bundle != null) {
+            // Retrieve the SMS Messages received
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+
+            // For every SMS message received
+            for (int i=0; i < msgs.length; i++) {
+                // Convert Object array
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                // Sender's phone number
+                str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
+                // Fetch the text message
+                str += msgs[i].getMessageBody().toString();
+                // Newline <img src="http://codetheory.in/wp-includes/images/smilies/simple-smile.png" alt=":-)" class="wp-smiley" style="height: 1em; max-height: 1em;">
+                str += "\n";
+            }
+
+            // Display the entire SMS Message
+            Log.d(TAG, str);
+        }
+    }
 }
