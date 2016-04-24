fc405fc125ca0bd3b0c55f60c26e641bf7407db4
justincasali <justin.casali@colorado.edu>
2016-04-24 16:37:45 -0600

    Works all in the MainActivity

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -1,19 +1,67 @@
 package ecen.devicecontroller;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView text;
+    IntentFilter filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+    private String TAG = MainActivity.class.getSimpleName();
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get the data (SMS data) bound to intent
+            Bundle bundle = intent.getExtras();
+
+            SmsMessage[] msgs = null;
+
+            String str = "";
+
+            if (bundle != null) {
+                // Retrieve the SMS Messages received
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                msgs = new SmsMessage[pdus.length];
+
+                // For every SMS message received
+                for (int i=0; i < msgs.length; i++) {
+                    // Convert Object array
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    // Sender's phone number
+                    str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
+                    // Fetch the text message
+                    str += msgs[i].getMessageBody().toString();
+                    // Newline <img src="http://codetheory.in/wp-includes/images/smilies/simple-smile.png" alt=":-)" class="wp-smiley" style="height: 1em; max-height: 1em;">
+                    str += "\n";
+                }
+
+                // Display the entire SMS Message
+                Log.d(TAG, str);
+
+                textMessage.setText(msgs[0].getMessageBody());
+                textAddress.setText(msgs[0].getOriginatingAddress());
+            }
+        }
+    };
+
+    private TextView textMessage;
+    private TextView textAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.textMessage);
+        textMessage = (TextView) findViewById(R.id.textMessage);
+        textAddress = (TextView) findViewById(R.id.textAddress);
+        this.registerReceiver(receiver, filter);
     }
 
 }
