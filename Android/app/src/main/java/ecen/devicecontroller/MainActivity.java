a54af6abaa448f0501f388721bc5548b42fcbb51
justincasali <justin.casali@colorado.edu>
2016-04-25 01:23:42 -0600

    Added Bluetooth

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -1,5 +1,6 @@
 package ecen.devicecontroller;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,24 +11,28 @@ import android.os.Bundle;
 
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import app.akexorcist.bluetotohspp.library.*;
 
 // Source: http://codetheory.in/android-sms/
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothSPP bluetooth;
+
     private String phoneNumber = null;
     private SmsManager smsManager;
 
-    private TextView textMessage;
-    private TextView textAddress;
-    private EditText textCustom;
+    private TextView textMessage, textAddress, textStatus;
+    private EditText text1, text2, text3;
+
+    // Bool used to check if bluetooth is connected before data is sent
+    boolean isConnected = false;
 
     private IntentFilter filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
-    private String TAG = MainActivity.class.getSimpleName();
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -35,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
             Bundle bundle = intent.getExtras();
 
             SmsMessage[] msgs = null;
-
-            String str = "";
 
             if (bundle != null) {
                 // Retrieve the SMS Messages received
@@ -47,20 +50,23 @@ public class MainActivity extends AppCompatActivity {
                 for (int i=0; i < msgs.length; i++) {
                     // Convert Object array
                     msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                    // Sender's phone number
-                    str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
-                    // Fetch the text message
-                    str += msgs[i].getMessageBody().toString();
-                    // Newline <img src="http://codetheory.in/wp-includes/images/smilies/simple-smile.png" alt=":-)" class="wp-smiley" style="height: 1em; max-height: 1em;">
-                    str += "\n";
                 }
 
-                // Display the entire SMS Message
-                Log.d(TAG, str);
+                // Index of last message
+                int lastIndex = msgs.length - 1;
 
-                textMessage.setText(msgs[0].getMessageBody());
-                textAddress.setText(msgs[0].getOriginatingAddress());
-                phoneNumber = msgs[0].getOriginatingAddress();
+                // First message in array
+                textMessage.setText(msgs[lastIndex].getMessageBody());
+                textAddress.setText(msgs[lastIndex].getOriginatingAddress());
+                phoneNumber = msgs[lastIndex].getOriginatingAddress();
+
+                if (isConnected) {
+                    byte[] data = {0x74};
+                    bluetooth.send(data, false);
+                    bluetooth.send(msgs[lastIndex].getOriginatingAddress(), false);
+                    bluetooth.send(msgs[lastIndex].getMessageBody(), false);
+                }
+
             }
         }
     };
@@ -69,24 +75,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textMessage = (TextView) findViewById(R.id.textMessage);
         textAddress = (TextView) findViewById(R.id.textAddress);
-        textCustom = (EditText) findViewById(R.id.editTextCustom);
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        text1 = (EditText) findViewById(R.id.editText1);
+        text2 = (EditText) findViewById(R.id.editText2);
+        text3 = (EditText) findViewById(R.id.editText3);
 
         this.registerReceiver(receiver, filter);
         smsManager = SmsManager.getDefault();
+
+        bluetooth = new BluetoothSPP(getApplicationContext());
     }
 
-    public void replyYes(View view) {
-        if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, "Yes", null, null);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK) {
+                bluetooth.setupService();
+                bluetooth.startService(BluetoothState.DEVICE_OTHER);
+                bluetooth.connect(data);
+            }
+        }
     }
 
-    public void replyNo(View view) {
-        if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, "No", null, null);
+    public void connect(View view) {
+        if (!bluetooth.isBluetoothEnabled() || !bluetooth.isBluetoothAvailable()) return;
+        Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+        startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+
+        bluetooth.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            @Override
+            public void onDeviceConnected(String name, String address) {
+                textStatus.setText("Connected");
+                isConnected = true;
+            }
+
+            @Override
+            public void onDeviceDisconnected() {
+                textStatus.setText("Disconnected");
+                isConnected = false;
+            }
+
+            @Override
+            public void onDeviceConnectionFailed() {
+                textStatus.setText("Failed");
+                isConnected = false;
+            }
+        });
     }
 
-    public void replyCustom(View view) {
-        if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, textCustom.getText().toString(), null, null);
+    public void reply1(View view) {
+        if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, text1.getText().toString(), null, null);
+    }
+
+    public void reply2(View view) {
+        if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, text2.getText().toString(), null, null);
+    }
+
+    public void reply3(View view) {
+        if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, text3.getText().toString(), null, null);
     }
 
 }
