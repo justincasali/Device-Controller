214341ed057537c9ec70257a07dbbabd24dd23dc
justincasali <justin.casali@colorado.edu>
2016-04-25 11:32:20 -0600

    Added phone calls & contact translation

--- Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
+++ Android/app/src/main/java/ecen/devicecontroller/MainActivity.java
@@ -1,6 +1,7 @@
 package ecen.devicecontroller;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,9 +10,13 @@ import android.os.CountDownTimer;
 import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.net.Uri;
+import android.os.Build;
+import android.database.Cursor;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     int lastIndex;
     SmsMessage[] msgs = null;
+    String contact = null;
 
     private BluetoothSPP bluetooth;
 
@@ -68,19 +74,52 @@ public class MainActivity extends AppCompatActivity {
                     byte[] data = {0x74};
                     bluetooth.send(data, false);
                     new CountDownTimer(500, 500) {
+                        @Override
                         public void onFinish() {
-                            bluetooth.send(msgs[lastIndex].getOriginatingAddress(), false);
+                            bluetooth.send(getContactName(msgs[lastIndex].getOriginatingAddress()), false);
                             new CountDownTimer(500, 500) {
+                                @Override
                                 public void onFinish() {
                                     bluetooth.send(msgs[lastIndex].getMessageBody(), false);
                                 }
-                                public void onTick(long mills) {
+
+                                @Override
+                                public void onTick(long millisUntilFinished) {
 
                                 }
-
                             }.start();
                         }
-                        public void onTick(long mills) {
+
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+
+                        }
+                    }.start();
+                }
+
+            }
+        }
+    };
+
+    private PhoneStateListener listener = new PhoneStateListener() {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            if (state == TelephonyManager.CALL_STATE_RINGING) {
+                contact = getContactName(incomingNumber);
+                textMessage.setText("Incoming Call");
+                textAddress.setText(contact);
+
+                if (isConnected) {
+                    byte[] data = {0x70};
+                    bluetooth.send(data, false);
+                    new CountDownTimer(500, 500) {
+                        @Override
+                        public void onFinish() {
+                            bluetooth.send(contact, false);
+                        }
+
+                        @Override
+                        public void onTick(long millisUntilFinished) {
 
                         }
                     }.start();
@@ -102,9 +141,15 @@ public class MainActivity extends AppCompatActivity {
         text2 = (EditText) findViewById(R.id.editText2);
         text3 = (EditText) findViewById(R.id.editText3);
 
+        // Text Messages
         this.registerReceiver(receiver, filter);
         smsManager = SmsManager.getDefault();
 
+        // Phone Calls
+        TelephonyManager manager = (TelephonyManager) getSystemService(Service.TELEPHONY_SERVICE);
+        manager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        // Bluetooth
         bluetooth = new BluetoothSPP(getApplicationContext());
     }
 
@@ -142,6 +187,22 @@ public class MainActivity extends AppCompatActivity {
                 isConnected = false;
             }
         });
+
+        // Reading data
+        bluetooth.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            @Override
+            public void onDataReceived(byte[] data, String message) {
+                //
+            }
+        });
+
+        // Send initial time data
+        if (isConnected) {
+            // Send special character and time info, device control one.
+            //byte[] data = {0x11}
+            //bluetooth.send();
+        }
+
     }
 
     public void reply1(View view) {
@@ -154,6 +215,41 @@ public class MainActivity extends AppCompatActivity {
 
     public void reply3(View view) {
         if (phoneNumber != null) smsManager.sendTextMessage(phoneNumber, null, text3.getText().toString(), null, null);
+    }
+
+    // Phone number to name converter
+    public String getContactName(final String phoneNumber)
+    {
+        Uri uri;
+        String[] projection;
+
+        if (Build.VERSION.SDK_INT >= 5)
+        {
+            uri = Uri.parse("content://com.android.contacts/phone_lookup");
+            projection = new String[] { "display_name" };
+        }
+        else
+        {
+            uri = Uri.parse("content://contacts/phones/filter");
+            projection = new String[] { "name" };
+        }
+
+        uri = Uri.withAppendedPath(uri, Uri.encode(phoneNumber));
+        Cursor cursor = this.getContentResolver().query(uri, projection, null, null, null);
+
+        String contactName = "";
+
+        if (cursor.moveToFirst())
+        {
+            contactName = cursor.getString(0);
+        }
+
+        cursor.close();
+        cursor = null;
+
+        if (contactName.equals("")) return phoneNumber;
+
+        return contactName;
     }
 
 }
